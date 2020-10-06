f1581bfd009378028e6b7cdbf4f5155a61c026b0
kultosh <kulgole90@gmail.com>
2020-10-06 07:12:26 +0545

    Created form upload using onChange event

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function UploadForm() {
+    const [img, setImg] = useState(null);
+    const [error, setError] = useState(null);
+    const types = ['image/png','image/jpeg'];
+
     const changeHandler = (e) => {
         let selected = e.target.files[0]
+        setImg(selected.name);
+        if(selected && types.includes(selected.type))
+        {
+            setImg(selected);
+            setError(null);
+        }
+        else
+        {
+            setImg(null);
+            setError('Something error!');
+        }
         console.log(selected);
     }
+
 
     return (
         <div>
             <form>
                 <input type="file" name="upload" onChange={changeHandler} />
             </form>
+            <div>{error && <div>{error}</div>}</div>
+            <span>{img && <div>{img.name}</div>}</span>
         </div>
     )
 }
