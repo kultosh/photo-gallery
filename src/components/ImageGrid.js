2e9f143444ec24c2432cf7242fb88ecd4d97cc96
kultosh <kulgole90@gmail.com>
2020-10-13 06:52:16 +0545

    Created animation for the image on upload

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -8,8 +8,8 @@ function ImageGrid({setSelectedImg}) {
     return (
         <div className="img-grid">
             {docs && docs.map(doc => (
-                <motion.div className="img-wrap" key={doc.id} whileHover={{opacity: 1}} onClick={() => setSelectedImg(doc.url)}>
-                    <img src={doc.url} alt="Uploading Img" />
+                <motion.div className="img-wrap" key={doc.id} layout whileHover={{opacity: 1}} onClick={() => setSelectedImg(doc.url)}>
+                    <motion.img src={doc.url} alt="Uploading Img" initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 1}} />
                 </motion.div>
             ))}
         </div>
